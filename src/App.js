fde6946aa313603e20b1296dc8bdd0d4858d367e
Noura-Darwazeh <noura.darwazeh11@gmail.com>
2023-12-08 17:43:14 -0500

    hihi11

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          hi, everyone
+          hi, 
         </p>
         <a
           className="App-link"
